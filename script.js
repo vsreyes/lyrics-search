ceb3a7b9863f64f7e7b6fc0a8f0953f77073eb1b
vsreyes <virgilio.s.reyes@gmail.com>
2020-09-01 22:34:46 +0200

    Added script

--- script.js
+++ script.js
@@ -73,5 +73,21 @@ form.addEventListener('submit', e => {
 
     const searchTerm = search.value.trim();
 
-    
+    if (!searchTerm) {
+        alert('Please type in a search term');
+    } else {
+        searchSongs(searchTerm);
+    }
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+    const clickedEl = e.target;
+
+    if (clickedEl.tagName === 'BUTTON') {
+        const artist = clickedEl.getAttribute('data-artist');
+        const songTitle = clickedEl.getAttribute('data-songtitle');
+
+        getLyrics(artist,songTitle);
+    }
 })
